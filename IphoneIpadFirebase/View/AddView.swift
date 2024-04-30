eac6b45b69d9399d0d7b3f77f6bff07583bb3f99
Reales Myles <rclarence123@gmail.com>
2024-04-29 23:55:44 -0500

    AddVie

---
+++ IphoneIpadFirebase/View/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  IphoneIpadFirebase
+//
+//  Created by Reales Rectoro Myles Clarence on 29/04/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
